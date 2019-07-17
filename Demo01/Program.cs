c80b60286f27f7ee9c0855c4dbfab7f716e4d497
Cristian Hamasaki <cristian.hamasaki@gmail.com>
2019-07-16 21:18:11 -0300

    - Primer ejemplo de consola y debug se escribio por consola y se freno la consola hast apresionar una tecla

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -8,8 +8,22 @@ namespace Demo01
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //Escribe por consola "Primer Ejercicio"
+            Console.WriteLine("Primer ejercicio");
+
+            /*Esta linea de comentarios
+                sirve para comentar varias
+                lineas  la vez*/
+            Console.WriteLine("segunda linea del primer ejercicio");
+
+            //Espera que el usuario teclee para terminar la aplicación 
+            //o continuar con la linea que seguiria
+            Console.ReadKey();
+
+            Console.WriteLine("segunda linea del primer ejercicio");
         }
     }
 }
